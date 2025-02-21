b7ea2f31b7e3e373705d9c5f807d7efc0b0d32b6
thelastcig <sonusharmaworkspace@gmail.com>
2025-02-21 13:46:04 +0530

    [Sonu Sharma] Add. UC-2 GET Request Method as Parameter

---
+++ app/src/main/java/com/messaging/app/HelloWorld.java
@@ -0,0 +1,30 @@
+package com.messaging.app;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("")
+public class HelloWorld {
+    //UC-1 GET Hello Request API
+    @GetMapping(value = {"/", "/hello",""})
+    public String hello(){
+        return "Hello from BridgeLabz!";
+    }
+    
+    //UC-2 GET Request Method and pass name as query parameter
+    @GetMapping("/query")    
+    public String sayHello(@RequestParam(defaultValue = "Sonu Sharma") String name){
+        return "Hello " + name + "!";
+    }
+
+   
+    
+    
+}
