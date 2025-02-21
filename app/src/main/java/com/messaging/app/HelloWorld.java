9825326a27480bda9928e29c19c457f9c07cc069
thelastcig <sonusharmaworkspace@gmail.com>
2025-02-21 16:48:03 +0530

    [Sonu Sharma] Add. PUT Request Method to display name

--- app/src/main/java/com/messaging/app/HelloWorld.java
+++ app/src/main/java/com/messaging/app/HelloWorld.java
@@ -1,11 +1,17 @@
 package com.messaging.app;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.Path;
+
+import javax.management.Query;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -35,6 +41,12 @@ public class HelloWorld {
     @PostMapping("/post")
     public String sayHelloPost(@RequestBody User user){
         return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz!";
+    }
+
+    //UC-5 Use PUT Request Method and pass first name as Path Variable and last name as Query Parameter
+    @PutMapping("/put/{firstName}")
+    public String sayHelloPut(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName){
+        return "Hello " + firstName + " " + lastName + " !";
     }
     
 }
